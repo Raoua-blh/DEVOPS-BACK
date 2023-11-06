a4348afd906ef13cfc4e70c0aa507b6b1d14b571
Raoua-blh <62572730+Raoua-blh@users.noreply.github.com>
2023-11-06 22:43:41 +0100

    Update perfomrance-test.js updatenbr of user

--- perfomrance-test.js
+++ perfomrance-test.js
@@ -1,20 +1,21 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-export const options = {
-
+export let options = {
+  vus: 10,              
   stages: [
-    { duration: '5m', target: 2 }
-
+    { duration: '30s', target: 10 },  
+    { duration: '1m', target: 10 },   
+    { duration: '30s', target: 0 }, 
   ],
 };
 
 export default () => {
-    const backendUrl = 'http://192.168.33.10:8082';
+    const backendUrl = 'http://192.168.33.10:8082/product';
   const backendResponse = http.get(backendUrl);
 
 
-  const frontendUrl = 'http://192.168.33.10:8088';
+  const frontendUrl = 'http://192.168.33.10:8088/products';
   const frontendResponse = http.get(frontendUrl);
 
   sleep(1);
